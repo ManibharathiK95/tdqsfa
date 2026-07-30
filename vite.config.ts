d2b9b7d9c8f87a4a8e456c09f49370a2f9b8969c
ManibharathiK95 <knandumech@gmail.com>
2026-07-30 12:40:03 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  return {
-    base: './',
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: '/tdqsfa/',
+})
