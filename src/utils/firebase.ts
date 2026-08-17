64aff0acf1bfa6d12358a5d907c8786d04ad5e4d
ManibharathiK95 <knandumech@gmail.com>
2026-08-17 15:16:38 +0400

    Update firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDRWg5hb3dVgEDhoGD-Jf5vTLb8i65h1XE",
@@ -9,7 +9,6 @@ const firebaseConfig = {
   storageBucket: "thulir-fa.firebasestorage.app",
   messagingSenderId: "322421499647",
   appId: "1:322421499647:web:e6cdd5941db36339d44eca"
-  measurementId: "G-1KY8GLT8FK"
 };
 
 const app = initializeApp(firebaseConfig);
